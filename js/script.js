59f63fd424507af0f7212426266a0752d0b5efc1
Arjan <69347644+skarjan@users.noreply.github.com>
2020-12-20 11:42:04 +0100

    Basic functionality and layout added
    - Able to generate a random letter
    - Made a simple and ugly layout.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,24 @@
-console.log("BoilerBootsBeta JS is linked");
+function displayWord () {
+  let letter = lowerCaseLetterGenerator();
+  let element  = document.getElementById("js-wordElement");
+
+    console.log(letter);
+}
+
+function lowerCaseLetterGenerator () {
+  const abc = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+  floatIndexOfLetter = Math.random()*26;
+  IntIndexOfLetter = Math.trunc(floatIndexOfLetter);
+  randomLowerChar = abc[IntIndexOfLetter];
+  console.log(randomLowerChar);
+}
+
+
+
+  function upperCaseLetterGenerator () {
+    const ABC =  ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+    floatIndexOfLetter = Math.random()*26;
+    IntIndexOfLetter = Math.trunc(floatIndexOfLetter);
+    randomLowerChar = abc[IntIndexOfLetter];
+    console.log(randomLowerChar);
+  }
