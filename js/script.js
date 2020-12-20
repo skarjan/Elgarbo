41f721f5f32e7eed5ec80e28461bd6ea4277f81e
Arjan <69347644+skarjan@users.noreply.github.com>
2020-12-20 11:43:55 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,12 @@
 function displayWord () {
   let letter = lowerCaseLetterGenerator();
   let element  = document.getElementById("js-wordElement");
-
-    console.log(letter);
+    element.innerHTML = letter;
 }
 
+// Random generatie van letters, cijfers en symbolen
+
+// Kleine letter genereren
 function lowerCaseLetterGenerator () {
   const abc = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
   floatIndexOfLetter = Math.random()*26;
@@ -15,6 +17,7 @@ function lowerCaseLetterGenerator () {
 
 
 
+// Hoofdletter genereren
   function upperCaseLetterGenerator () {
     const ABC =  ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
     floatIndexOfLetter = Math.random()*26;
