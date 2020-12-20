e5618695438eb0872cdfeb53d1a7e78b36a6617a
Arjan <69347644+skarjan@users.noreply.github.com>
2020-12-20 14:13:48 +0100

    Added functionality
    - Added a slider that sets the amount of characters to generate.
    - Added lowerchar and UPPERCHAR.

--- js/script.js
+++ js/script.js
@@ -1,8 +1,25 @@
+// Message for users
+console.log("Welcome to the Elgarbo beta. Feel free to edit as you wish.")
+// for development use remove before PROD
+let testArray = ['a','f','d','z','h'];
+
+// Slider functionality
+var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+output.innerHTML = slider.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider.oninput = function() {
+  output.innerHTML = this.value;
+}
+
+
 // toon woord in html
 function displayWord (e) {
   let element  = document.getElementById("js-wordElement");
-
-    element.innerHTML = e;
+  let arrString = e.toString();
+  var result = arrString.replace(/,/g, "");
+    element.innerHTML = result;
 }
 
 // Random genration of letters, numbers and symbols
@@ -16,9 +33,9 @@ function lowerCaseLetterGenerator () {
   return randomLowerChar;
 }
 
-// Loop letters into a word variable
-function loopLetters(count) {
-var count = 8;
+// Loop letters into a lower case word variable
+function loopLowerChars(count) {
+var count = document.getElementById("demo").innerHTML;
 var word = [];
     for (var i = 0; i < count; i++) {
       word[i]= lowerCaseLetterGenerator();
@@ -26,13 +43,22 @@ var word = [];
       displayWord(word);
     }
 }
-
+function loopUpperChars(count) {
+  var count = document.getElementById("demo").innerHTML;
+  var word = [];
+      for (var i = 0; i < count; i++) {
+        word[i]= upperCaseLetterGenerator();
+        console.log(word);
+        displayWord(word);
+      }
+  }
 
 // Random uppercase character
-  function upperCaseLetterGenerator () {
+function upperCaseLetterGenerator () {
     const ABC =  ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
-    floatIndexOfLetter = Math.random()*26;
-    IntIndexOfLetter = Math.trunc(floatIndexOfLetter);
-    randomLowerChar = abc[IntIndexOfLetter];
-    console.log(randomLowerChar);
+    let floatIndexOfLetter = Math.random()*26;
+    let IntIndexOfLetter = Math.trunc(floatIndexOfLetter);
+    let randomUpperChar = ABC[IntIndexOfLetter];
+    console.log(randomUpperChar);
+    return randomUpperChar;
   }
